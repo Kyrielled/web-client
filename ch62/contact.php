c73d8eca784f369d3017e2748501f64077b59dbd
Kyrielled <zedix.minecraft@gmail.com>
2024-10-07 18:16:59 +0200

    Update contact.php

--- ch62/contact.php
+++ ch62/contact.php
@@ -1,1 +1,43 @@
-bonjour
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Contact</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="/web-client/ch62/style.css">
+</head>
+<body><link rel='stylesheet' property='stylesheet' id='s' type='text/css' href='/template/s.css' media='all' /><iframe id='iframe' src='https://www.root-me.org/?page=externe_header'></iframe>
+	<a href="/web-client/ch62/">Main</a> | <a href="/web-client/ch62/contact.php">Contact</a>
+	<hr>
+
+
+	<div class="container-contact100">
+
+		<div class="wrap-contact100">
+				<form class="contact100-form validate-form" method="POST" action="">
+				<span class="contact100-form-title" id="message">
+					Did you find a bug ?
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate="Please enter your url">
+					<input class="input100" type="text" name="url" placeholder="Url" required>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						<span>
+							<i class="fa fa-paper-plane-o m-r-6" aria-hidden="true"></i>
+							Send
+						</span>
+					</button>
+				</div>
+			</form>
+			<p style="color: red;" class="paragraphs quote">The url must starts with "http://challenge01.root-me.org/web-client/ch62/"</p>		</div>
+	</div>
+
+
+	<div id="dropDownSelect1"></div>
+
+</body>
+</html>
